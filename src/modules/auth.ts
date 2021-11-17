5025eeb8873685cf9c5a962bd541dbac2330df95
Dusty Paper Box <sora1211x@gamil.com>
2021-11-17 10:45:15 +0000

    Add auth module

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,18 +1,64 @@
+import produce from 'immer';
 import { ActionType, createAction, createReducer } from 'typesafe-actions';
 
-export const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+export const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+export const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-export const sampleAction = createAction(SAMPLE_ACTION)();
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({
+    form,
+    key,
+    value,
+  }: {
+    form: keyof AuthState;
+    key: keyof AuthState[keyof AuthState];
+    value: string;
+  }) => ({
+    form,
+    key,
+    value,
+  }),
+)();
+export const initializeForm = createAction(
+  INITIALIZE_FORM,
+  (form: keyof AuthState) => form,
+)();
 
-export type AuthState = {};
-
-const actions = { sampleAction };
+const actions = { changeField, initializeForm };
 export type AuthAction = ActionType<typeof actions>;
 
-const initialState: AuthState = {};
+export type AuthState = {
+  register: {
+    username: string;
+    password: string;
+    passwordConfirm: string;
+  };
+  login: {
+    username: string;
+    password: string;
+  };
+};
+
+const initialState: AuthState = {
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  login: {
+    username: '',
+    password: '',
+  },
+};
 
 const auth = createReducer<AuthState, AuthAction>(initialState, {
-  [SAMPLE_ACTION]: (state, action) => state,
+  [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+    produce(state, (draft) => {
+      draft[form][key] = value;
+    }),
+  [INITIALIZE_FORM]: (state, { payload: form }) => ({
+    ...state,
+    [form]: initialState[form],
+  }),
 });
-
-export default auth;
